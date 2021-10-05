bf9a4fa4a2099807584649730810f53cb0ccd66b
phplpsummer <sh052100@naver.com>
2021-10-05 15:52:52 +0900

    스크립트 작업

---
+++ js/custom.js
@@ -0,0 +1,14 @@
+let num = 0
+
+setInterval(function(){
+
+    //현재 위치값이 -120보다 작아지면(프레임 밖으로 벗어나면)
+    if(num < -120){
+        num = 0;  //0으로 초기화 후
+        $(".list").children("li").eq(0).appendTo(".list");  //맨 앞요소를 맨뒤로 보냄
+    }
+
+    num-=2;
+    $(".list").css({left:num});
+
+},40)
