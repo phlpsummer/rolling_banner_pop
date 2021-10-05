a249d5acb8dc8df66239fb40b3340e788dc64520
phplpsummer <sh052100@naver.com>
2021-10-05 16:24:01 +0900

    마우스오버시 rolling정지+함수설정

--- js/custom.js
+++ js/custom.js
@@ -1,7 +1,16 @@
 let num = 0
+let timer;
 
-setInterval(function(){
+timer = setInterval(rolling,40);
 
+$(".list").on("mouseenter",function(){
+    clearInterval(timer);
+});
+$(".list").on("mouseleave",function(){
+    timer = setInterval(rolling,40);
+});
+
+function rolling(){
     //현재 위치값이 -120보다 작아지면(프레임 밖으로 벗어나면)
     if(num < -120){
         num = 0;  //0으로 초기화 후
@@ -10,5 +19,4 @@ setInterval(function(){
 
     num-=2;
     $(".list").css({left:num});
-
-},40)
+}
