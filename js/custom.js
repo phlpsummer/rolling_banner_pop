5881d9cb00b7d1cb69e27d179f239e76805c58ab
phplpsummer <sh052100@naver.com>
2021-10-06 15:36:43 +0900

    ajax 추가

--- js/custom.js
+++ js/custom.js
@@ -27,6 +27,19 @@ $(".list li").on("click",function(e){
                     .text("close")
             )
     )
+
+    let target = $(this).children("a").attr("href");
+
+    $.ajax({
+        url:target,
+        success:function(data){
+            console.log(data);
+            $(".pop .con").html(data);
+        },
+        error:function(err){
+            console.error("데이터를 불러오는 데 실패했습니다.");
+        }
+    })
 });
 
 //닫기 버튼 클릭시 -제거-
@@ -40,7 +53,7 @@ $("body").on("click",".pop .btnClose", function(){
 
 //함수 정의
 function rolling(){
-    //현재 위치값이 -120보다 작아지면(프레임 밖으로 벗어나면)
+    //현재 위치값이 -240(썸네일 하나 크기)보다 작아지면 (프레임 밖으로 벗어나면)
     if(num < -240){
         num = 0;  //0으로 초기화 후
         $(".list").children("li").eq(0).appendTo(".list");  //맨 앞요소를 맨뒤로 보냄
